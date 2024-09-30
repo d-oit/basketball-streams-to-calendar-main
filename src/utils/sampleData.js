c9bcc0d3237e4d9b9a8a2bac56e8e74a7d6bb2d4
Dominik Oswald <6849456+d-oit@users.noreply.github.com>
2024-09-30 11:22:36 +0200

    add sampledata

---
+++ src/utils/sampleData.js
@@ -0,0 +1,45 @@
+const sampleData = `20.09.24
+19:15
+ProA: VET-CONCEPT Gladiators Trier vs. Tigers Tübingen
+VET-CONCEPT Gladiators Trier
+VET-CONCEPT Gladiators Trier
+
+Basketball
+VET-CONCEPT Gladiators Trier
+vs.
+Tigers Tübingen
+20.09.24
+19:45
+ProA: EPG GUARDIANS Koblenz vs. VfL SparkassenStars Bochum
+EPG Guardians Koblenz
+EPG Guardians Koblenz
+
+Basketball
+EPG Guardians Koblenz
+vs.
+VfL SparkassenStars Bochum
+21.09.24
+18:30
+ProA: Phoenix Hagen vs. Science City Jena
+Phoenix Hagen
+Phoenix Hagen
+
+Basketball
+Phoenix Hagen
+vs.
+Science City Jena
+21.09.24
+18:45
+ProA: Bozic Estriche Knights Kirchheim vs. GIESSEN 46ers
+Bozic Estriche Knights Kirchheim
+Bozic Estriche Knights Kirchheim
+
+Basketball
+Bozic Estriche Knights Kirchheim
+vs.
+GIESSEN 46ers
+21.09.24
+18:45
+ProA: BBC Bayreuth vs. HAKRO Merlins Crailsheim`;
+
+export default sampleData;
