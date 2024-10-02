dc3a4a78612034f2865df10659b6ef36828300a6
Dominik Oswald <6849456+d-oit@users.noreply.github.com>
2024-10-02 10:45:12 +0200

    update gemini prompt

--- src/utils/geminiApi.js
+++ src/utils/geminiApi.js
@@ -9,15 +9,15 @@ export const GeminiPrompt = 'Analyze the following basketball live stream text a
   'Use the year ' + currentYear + ' is no year is included in a date.\n' +
   'Ignore dates which the word Eishockey, Fussball, Volleyball included in a date description.\n' +
   'Do not create the same event title on the same date. Always use "vs" if you need to insert a separating word.\n' +
-  'Use english as english \n' +
+  'Location is magentasport.de, welt.tv, ran.de \n' +
   "Don't format the json with markdown, only plaintext:\n\n" +
   '{text}\n\n' +
   'Please provide the following information in JSON:\n' +
   '- eventTitle: String\n' +
   '- startDateTime: use the extract date time \n' +
   '- endDateTime: use the extract date time (if not specified, add 1,5 hours)\n' +
-  '- location: String (if available)\n' +
-  '- description: String (summary of the event)\n\n' +
+  '- location: String (use a found url as location if empty)\n' +
+  '- description: String (summary of the event) - add a description by yourself if is empty\n\n' +
   'Ensure that the response is a valid JSON object.';
 
 export const analyzeWithGemini = async (text, customPrompt) => {
