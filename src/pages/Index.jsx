af192688971a727885136e832c233446672dffc7
Dominik Oswald <6849456+d-oit@users.noreply.github.com>
2024-10-04 16:43:00 +0200

    Aktualisieren von Index.jsx

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -12,7 +12,7 @@ import { version } from '@/utils/version';
 
 const Copyright = () => (
   <div className="text-sm text-gray-600 dark:text-gray-400 mr-4">
-    <span>Copyright © 2024 <svg className="h-8 w-8 text-orange-500 inline" width="16" height="16" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />  <path d="M3 21h4l13 -13a1.5 1.5 0 0 0 -4 -4l-13 13v4" />  <line x1="14.5" y1="5.5" x2="18.5" y2="9.5" />  <polyline points="12 8 7 3 3 7 8 12" />  <line x1="7" y1="8" x2="5.5" y2="9.5" />  <polyline points="16 12 21 17 17 21 12 16" />  <line x1="16" y1="17" x2="14.5" y2="18.5" /></svg>  <a href="https://d-oit.github.io" className="underline hover:text-gray-800 dark:hover:text-gray-200">d.o.it</a> <HelpDialog /> </span>
+    <span>Created 2024 <svg className="h-8 w-8 text-orange-500 inline" width="16" height="16" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />  <path d="M3 21h4l13 -13a1.5 1.5 0 0 0 -4 -4l-13 13v4" />  <line x1="14.5" y1="5.5" x2="18.5" y2="9.5" />  <polyline points="12 8 7 3 3 7 8 12" />  <line x1="7" y1="8" x2="5.5" y2="9.5" />  <polyline points="16 12 21 17 17 21 12 16" />  <line x1="16" y1="17" x2="14.5" y2="18.5" /></svg>  <a href="https://d-oit.github.io" className="underline hover:text-gray-800 dark:hover:text-gray-200">d.o.it</a> <HelpDialog /> </span>
   </div>
 );
 
